4e9bdee1776390fd44d054eb26789370d4685c25
Talha Zahid <italhazahid@gmail.com>
2025-11-26 13:04:32 +0500

    Completed Welcome Screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,15 +1,52 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '../../components/ScreenWrapper'
+import Typo from '@/components/Typo'
+import { colors, spacingX, spacingY } from '@/constants/theme'
+import { verticalScale } from '@/utils/styling'
+import Animated, { FadeIn } from 'react-native-reanimated'
+import Button from '@/components/Button'
 
 const WelcomeScreen = () => {
   return (
     <ScreenWrapper showPattern={true}>
-      <Text></Text>
+      <View style={styles.container}>
+        <View style={{ alignItems: 'center' }}>
+          <Typo color={colors.white} size={43} fontWeight={'900'}>ChatX</Typo>
+        </View>
+        <Animated.Image
+        entering={FadeIn.duration(700).springify()}
+        source={require('../../assets/images/welcome.png')}
+        style={styles.welcomeImage}
+        resizeMode='contain'
+        />
+        <View>
+          <Typo color={colors.white} size={33} fontWeight={'800'} >Connect with your Friends and Family easily.</Typo>
+        </View>
+        <Button >
+          <Typo size={23} fontWeight={'bold'} >Get Started</Typo>
+        </Button>
+      </View>
     </ScreenWrapper>
   )
 }
 
 export default WelcomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingHorizontal: spacingX._20,
+    marginVertical: spacingY._25,
+  },
+  background: {
+    flex: 1,
+    backgroundColor: colors.neutral900,
+  },
+  welcomeImage: {
+    height: verticalScale(300),
+    aspectRatio: 1,
+    alignSelf: 'center'
+  }
+})
