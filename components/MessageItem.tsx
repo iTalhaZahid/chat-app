a84d6c61a481f6c1838945f184b580f747c25ec4
Talha Zahid <italhazahid@gmail.com>
2025-12-08 23:58:38 +0500

    Completed Message Item

---
+++ components/MessageItem.tsx
@@ -0,0 +1,82 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { MessageProps } from '@/types'
+import { useAuth } from '@/context/authContext';
+import { colors, radius, spacingX, spacingY } from '@/constants/theme';
+import { verticalScale } from '@/utils/styling';
+import Avatar from './Avatar';
+import Typo from './Typo';
+import moment from 'moment';
+import { Image } from 'expo-image';
+
+const MessageItem = ({
+    item, isDirect
+}: { item: MessageProps, isDirect: boolean }) => {
+
+    const formattedDate = moment(item.createdAt).isSame(moment(), 'day') ? moment(item.createdAt).format('hh:mm A') : moment(item.createdAt).format('MMM D,h:mm A');
+
+    const { user: currentUser } = useAuth();
+    const isMe = currentUser?.id == item?.sender?.id;
+    return (
+        <View style={[styles.messageContainer, isMe ? styles.myMessage : styles.theirMessage]}>
+            {
+                !isMe && !isDirect && (
+                    <Avatar size={30} uri={item?.sender?.avatar} style={styles.messageAvatar} />
+                )
+            }
+
+            <View style={[styles.messageBubble, isMe ? styles.myBubble : styles.theirBubble]}>
+
+                {!isMe && !isDirect && (
+                    <Typo color={colors.neutral900} size={13} fontWeight={'600'}>{item.sender.name}</Typo>
+                )}
+
+                {item.attachment && (
+                    <Image
+                        source={{ uri: item.attachment }}
+                        contentFit="cover"
+                        style={styles.attachment}
+                        transition={100}
+                    />
+                )}
+                {item.content && <Typo size={15}>{item.content}</Typo>}
+                <Typo style={{ alignSelf: 'flex-end' }} size={11} fontWeight={"500"}>{formattedDate}</Typo>
+            </View>
+        </View>
+    )
+}
+
+export default MessageItem
+
+const styles = StyleSheet.create({
+    messageContainer: {
+        flexDirection: 'row',
+        gap: spacingX._7,
+        maxWidth: '80%',
+    },
+    myMessage: {
+        alignSelf: 'flex-end',
+    },
+    theirMessage: {
+        alignSelf: 'flex-start',
+    },
+    messageAvatar: {
+        alignSelf: 'flex-end',
+    },
+    attachment: {
+        height: verticalScale(180),
+        width: verticalScale(180),
+        borderRadius: radius._10,
+    },
+    messageBubble: {
+        padding: spacingX._10,
+        borderRadius: radius._15,
+        gap: spacingY._5,
+    },
+    myBubble: {
+        backgroundColor: colors.myBubble
+    },
+    theirBubble: {
+        backgroundColor: colors.otherBubble
+    }
+})
